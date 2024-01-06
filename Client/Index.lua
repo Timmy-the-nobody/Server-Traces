0eea59ed554249deab7eefdc0233f8137fe41d74
Timmy <timmy.the.nobody@gmail.com>
2024-01-06 17:08:08 +0100

    refactor/feat: server traces v2.0.0
    - Support all trace functions
    - Safe trace queries
    - Better memory management

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,9 +1,42 @@
-local traceLineSingle = Client.TraceLineSingle
+local table = table
+local Trace = Trace
+local Events = Events
 
---[[ Trace:Request ]]--
-local function onTraceRequested(tStart, tEnd, iChannel, iTraceMode, tIgnoredActors, iTraceID)
-    local tTrace = traceLineSingle(tStart, tEnd, iChannel, iTraceMode, tIgnoredActors)
-    Events.CallRemote("Trace:Return", tTrace, iTraceID)
+local tTraceTypes = {
+    [1] = Trace.BoxMulti,
+    [2] = Trace.BoxSingle,
+    [3] = Trace.CapsuleMulti,
+    [4] = Trace.CapsuleSingle,
+    [5] = Trace.LineMulti,
+    [6] = Trace.LineSingle,
+    [7] = Trace.SphereMulti,
+    [8] = Trace.SphereSingle,
+}
+
+local function handleTrace(...)
+    local tArgs = {...}
+
+    local sID = tArgs[1]
+
+    local iTraceType = tArgs[2]
+    if not tTraceTypes[iTraceType] then return print("flop") end
+
+    local tRest = {}
+    for i = 3, #tArgs do
+        tRest[#tRest + 1] = tArgs[i]
+    end
+
+    local function doTrace()
+        return tTraceTypes[iTraceType](table.unpack(tRest))
+    end
+
+    local bSuccess, xResultOrError = pcall(doTrace)
+    if not bSuccess then
+        Events.CallRemote("SVTr:Error", sID, xResultOrError:match(":%s(.+)$"))
+        return
+    end
+
+    Events.CallRemote("SVTr:Pong", sID, xResultOrError)
 end
 
-Events.SubscribeRemote("Trace:Request", onTraceRequested)
+Events.SubscribeRemote("SVTr", function(...) handleTrace(...) end)
--- Server/Index.lua
+++ Server/Index.lua
@@ -1,98 +1,279 @@
+SVTrace = SVTrace or {}
+
 local ipairs = ipairs
 local type = type
 local getmetatable = getmetatable
 local mathRandom = math.random
-local isA = NanosUtils.IsA
 
-local iLastTraceID = 0
-local tReadyPlayers = {}
+local tAllPlayers = {}
 local tPendingTraces = {}
 
---[[ Player Ready ]]--
-local function onPlayerReady(pPlayer)
-    tReadyPlayers[#tReadyPlayers + 1] = pPlayer
-end
-
-Player.Subscribe("Ready", onPlayerReady)
-
---[[ Package Load ]]--
-Package.Subscribe("Load", function()
-    for _, v in ipairs(Player.GetAll()) do
-        onPlayerReady(v)
-    end
+-- Player: "Spawn"
+Player.Subscribe("Spawn", function(pPlayer)
+    tAllPlayers[#tAllPlayers + 1] = pPlayer
 end)
 
---[[ Player Destroy ]]--
+-- Package: "Load"
+Package.Subscribe("Load", function()
+    tAllPlayers = Player.GetAll()
+end)
+
+-- Player: "Destroy"
 Player.Subscribe("Destroy", function(pPlayer)
     local tNewPlayers = {}
-    for _, v in ipairs(tReadyPlayers) do
+    for _, v in ipairs(tAllPlayers) do
         if (v ~= pPlayer) then
             tNewPlayers[#tNewPlayers + 1] = pPlayer
         end
     end
-    tReadyPlayers = tNewPlayers
+    tAllPlayers = tNewPlayers
+
+    -- Clears the traces queried to this player
+    for k, v in pairs(tPendingTraces) do
+        if (v.authority == pPlayer) then
+            tPendingTraces[k] = nil
+        end
+    end
 end)
 
---[[ onTraceReturn ]]--
-local function onTraceReturn(pPlayer, tTrace, iTraceID)
-    if not iTraceID or not tPendingTraces[iTraceID] then
-        return
+-- Events: "SVTr:Pong"
+Events.SubscribeRemote("SVTr:Pong", function(pPlayer, sID, tTrace)
+    if not sID or not tPendingTraces[sID] then return end
+    if (tPendingTraces[sID].authority ~= pPlayer) then return end
+
+    tPendingTraces[sID].callback(tTrace)
+    tPendingTraces[sID] = nil
+end)
+
+-- Events: "SVTr:Error"
+Events.SubscribeRemote("SVTr:Error", function(pPlayer, sID, sError)
+    if not sID or not tPendingTraces[sID] then return end
+    if (tPendingTraces[sID].authority ~= pPlayer) then return end
+
+    Console.Error(sError)
+    tPendingTraces[sID] = nil
+end)
+
+---`🔹 Server`<br>
+---Generates a random string, used for trace IDs
+---@param iLen number @The length of the string
+---@return string @The random string
+---
+local function generateRandomString(iLen)
+    local sRand = ""
+    for _ = 1, iLen do
+        sRand = sRand..string.char(mathRandom(48, 122))
     end
 
-    if (tPendingTraces[iTraceID].authority ~= pPlayer) then
-        return
+    if tPendingTraces[sRand] then
+        return generateRandomString(iLen)
     end
 
-    tPendingTraces[iTraceID].callback(tTrace)
-    tPendingTraces[iTraceID] = nil
+    return sRand
 end
 
-Events.SubscribeRemote("Trace:Return", onTraceReturn)
-
---[[ Trace ]]--
-function Trace(callback, tStart, tEnd, iChannel, iTraceMode, tIgnoredActors, pForceAutority)
-    if (#tReadyPlayers == 0) then
-        Package.Warn("At least 1 ready client needed to perform a trace")
+---`🔹 Server`<br>
+---Handles the creation of a trace query
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---@return Player? @The authority player, or nil if failed
+---@return string? @The trace unique ID, or nil if failed
+---
+local function handleTraceQuery(callback, pForceAutority)
+    if (type(callback) ~= "function") then
+        Console.Warn("SVTrace: Callback must be a function")
         return
     end
 
-    if not callback or (type(callback) ~= "function") then
-        Package.Warn("Callback parameter is invalid (function expected)")
+    if (#tAllPlayers == 0) then
+        Console.Warn("SVTrace: No players found to perform a trace")
         return
     end
-
-    if not tStart or (getmetatable(tStart) ~= Vector) then
-        Package.Warn("Trace start parameter is invalid (Vector expected)" )
-        return
-    end
-
-    if not tEnd or (getmetatable(tEnd) ~= Vector) then
-        Package.Warn("Trace end parameter is invalid (Vector expected)")
-        return
-    end
-
-    iLastTraceID = (iLastTraceID + 1)
 
     local pAuthority
-    if pForceAutority and isA(pForceAutority, Player) then
+    if pForceAutority and (getmetatable(pForceAutority) == Player) then
         pAuthority = pForceAutority
     else
-        pAuthority = tReadyPlayers[mathRandom(1, #tReadyPlayers)]
+        pAuthority = tAllPlayers[mathRandom(1, #tAllPlayers)]
     end
 
-    tPendingTraces[iLastTraceID] = {
-        ["authority"] = pAuthority,
-        ["callback"] = callback
-    }
+    if not pAuthority then
+        Console.Warn("SVTrace: No authority player found to perform a trace")
+        return
+    end
 
-    Events.CallRemote("Trace:Request", pAuthority,
-        tStart,
-        tEnd,
-        (iChannel or CollisionChannel.WorldStatic),
-        (iTraceMode or 0),
-        (tIgnoredActors or {}),
-        iLastTraceID
-    )
+    local sID = generateRandomString(4)
+    tPendingTraces[sID] = {authority = pAuthority, callback = callback}
+
+    return pAuthority, sID
 end
 
-Package.Export("Trace", Trace)
+---`🔹 Server`<br>
+---Trace a box against the world using object types and return overlapping hits and then first blocking hit<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.<br>
+---Results are sorted, so a blocking hit (if found) will be the last element of the array<br>
+---Only the single closest blocking result will be generated, no tests will be done after that
+---@param tStart Vector @Start location of the box
+---@param tEnd Vector @End location of the box
+---@param tHalfSize Vector @Distance from the center of box along each axis
+---@param tRot Rotator @Orientation of the box
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.BoxMulti(tStart, tEnd, tHalfSize, tRot, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 1, tStart, tEnd, tHalfSize, tRot, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a box against the world and returns a table with the first blocking hit information<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.
+---@param tStart Vector @Start location of the box
+---@param tEnd Vector @End location of the box
+---@param tHalfSize Vector @Distance from the center of box along each axis
+---@param tRot Rotator @Orientation of the box
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.BoxSingle(tStart, tEnd, tHalfSize, tRot, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 2, tStart, tEnd, tHalfSize, tRot, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a capsule against the world using object types and return overlapping hits and then first blocking hit<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.<br>
+---Results are sorted, so a blocking hit (if found) will be the last element of the array<br>
+---Only the single closest blocking result will be generated, no tests will be done after that
+---@param tStart Vector @Start location of the capsule
+---@param tEnd Vector @End location of the capsule
+---@param fRad number @Radius of the capsule to sweep
+---@param fHalfHeight number @Distance from center of capsule to tip of hemisphere endcap.
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.CapsuleMulti(tStart, tEnd, fRad, fHalfHeight, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 3, tStart, tEnd, fRad, fHalfHeight, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a capsule against the world and returns a table with the first blocking hit information<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.
+---@param tStart Vector @Start location of the capsule
+---@param tEnd Vector @End location of the capsule
+---@param fRad number @Radius of the capsule to sweep
+---@param fHalfHeight number @Distance from center of capsule to tip of hemisphere endcap.
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.CapsuleSingle(tStart, tEnd, fRad, fHalfHeight, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 4, tStart, tEnd, fRad, fHalfHeight, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a ray against the world using object types and return overlapping hits and then first blocking hit<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.<br>
+---Results are sorted, so a blocking hit (if found) will be the last element of the array<br>
+---Only the single closest blocking result will be generated, no tests will be done after that
+---@param tStart Vector @Start location of the ray
+---@param tEnd Vector @End location of the ray
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.LineMulti(tStart, tEnd, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 5, tStart, tEnd, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a ray against the world and returns a table with the first blocking hit information<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.
+---@param tStart Vector @Start location of the ray
+---@param tEnd Vector @End location of the ray
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.LineSingle(tStart, tEnd, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 6, tStart, tEnd, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a sphere against the world using object types and return overlapping hits and then first blocking hit<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.<br>
+---Results are sorted, so a blocking hit (if found) will be the last element of the array<br>
+---Only the single closest blocking result will be generated, no tests will be done after that
+---@param tStart Vector @Start location of the sphere
+---@param tEnd Vector @End location of the sphere
+---@param fRad number @Radius of the sphere
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.SphereMulti(tStart, tEnd, fRad, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 7, tStart, tEnd, fRad, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+---`🔹 Server`<br>
+---Trace a sphere against the world and returns a table with the first blocking hit information<br>
+---Note: The Trace will collide with the ObjectType (in the Collision Settings), even if the channel is ignored below.
+---@param tStart Vector @Start location of the sphere
+---@param tEnd Vector @End location of the sphere
+---@param fRad number @Radius of the sphere
+---@param iCollisionChannel? number @Supports several channels separating by `|` (using bit-wise operations)
+---@param iTraceMode? number @Trace Mode, pass all parameters separating by `|` (using bit-wise operations)
+---@param tIgnoredActors? table @Array of actors to ignore during the trace
+---@param callback function @The callback
+---@param pForceAutority? Player @The player to force the call to be done on
+---
+function SVTrace.SphereSingle(tStart, tEnd, fRad, iCollisionChannel, iTraceMode, tIgnoredActors, callback, pForceAutority)
+    local pAuthority, sID = handleTraceQuery(callback, pForceAutority)
+    if not pAuthority then return end
+
+    Events.CallRemote("SVTr", pAuthority, sID, 8, tStart, tEnd, fRad, iCollisionChannel, iTraceMode, tIgnoredActors)
+end
+
+-- Timer.SetTimeout(function()
+--     SVTrace.LineSingle(Vector(0, 0, 100), Vector(0, 0, -100), CollisionChannel.WorldStatic, TraceMode["DrawDebug"], nil, function(tRes)
+--         print(NanosTable.Dump(tRes))
+--     end)
+-- end, 5000)
+
+Package.Export("SVTrace", SVTrace)
